387c0048684db5a886e8434a82bc9e45e6857f42
jrm-muller <jerome.mlr40@gmail.com>
2023-04-01 10:57:28 +0200

    fix image loading 3

---
+++ _next/static/eIX_xmLp1bLBU4OiCmpu4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4f458ab0543294ff.css","static/chunks/700-55ddd0025376bdb1.js","static/chunks/pages/index-d5d325912b4e5a02.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
